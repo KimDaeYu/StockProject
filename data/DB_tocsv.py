ef14c822662d5017c95fd174650ea1bdf0cbdab9
김대유 <dea0323@gmail.com>
2020-09-24 14:27:55 +0000

    리눅스상 파일 내보내기는 가능하나 원격데이터베이스에서 가져오는것은 불가능 -> pandas로 불러와서 저장해야함

--- data/DB_tocsv.py
+++ data/DB_tocsv.py
@@ -9,55 +9,90 @@ KOSPI = 1
 KOSDAQ = 2
 KONEX = 4
 
-def Get_invest_csv(sector, date = 0):
+def Get_invest_csv(sector,expect, date = 0):
     con = Connect_DB()
     cursor = con.cursor(pymysql.cursors.DictCursor)
-    if (sector % 2 is 1):
-        filename = "kospi_invest.csv"
-        table = "Kospi_invest_info"
-        cursor.execute("""
-           SELECT * INTO OUTFILE %s FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\\\' LINES TERMINATED BY '\\n' FROM """ + table + """;
-          """, [filename])
-        con.commit()
-        shutil.copy("/var/lib/mysql/" + Get_DB() + "/" + filename,                                         "/workspace/stock_project3/execl/" + str(date) + '_' + filename)
-        file = '/var/lib/mysql/study_db/' + filename
-        if os.path.isfile(file):
-            os.remove(file)
+    path = str(os.path.abspath(__file__))[:-16] + "execl/" + str(date) + '_'
+    if (sector % 2 == 1):
+        if(expect == "E"):
+            filename = "kospi_invest.csv"
+            table = "Kospi_exp_invest_info"
+            cursor.execute("""
+               SELECT * INTO OUTFILE %s FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\\\' LINES TERMINATED BY '\\n' FROM """ + table + """;
+              """, [filename])
+            con.commit()
+            shutil.copy("/var/lib/mysql/" + Get_DB() + "/" + filename,  path + filename)
+            file = '/var/lib/mysql/study_db/' + filename
+            if os.path.isfile(file):
+                os.remove(file)
+        else:
+            filename = "kospi_invest.csv"
+            table = "Kospi_invest_info"
+            cursor.execute("""
+               SELECT * INTO OUTFILE %s FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\\\' LINES TERMINATED BY '\\n' FROM """ + table + """;
+              """, [filename])
+            con.commit()
+            shutil.copy("/var/lib/mysql/" + Get_DB() + "/" + filename, path + filename)
+            file = '/var/lib/mysql/study_db/' + filename
+            if os.path.isfile(file):
+                os.remove(file)
 
     sector /= 2
     sector = int(sector)
-    if (sector % 2 is 1):
-        filename = "kosdaq_invest.csv"
-        table = "Kosdaq_invest_info"
-        cursor.execute("""
-           SELECT * INTO OUTFILE %s FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\\\' LINES TERMINATED BY '\\n' FROM """ + table + """;
-          """, [filename])
-        shutil.copy("/var/lib/mysql/" + Get_DB() + "/" + filename,                                         "/workspace/stock_project3/execl/" + str(date) + '_' + filename)
-        file = '/var/lib/mysql/study_db/' + filename
-        if os.path.isfile(file):
-            os.remove(file)
+    if (sector % 2 == 1):
+        if(expect == "E"):
+            filename = "kosdaq_invest.csv"
+            table = "Kosdaq_exp_invest_info"
+            cursor.execute("""
+               SELECT * INTO OUTFILE %s FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\\\' LINES TERMINATED BY '\\n' FROM """ + table + """;
+              """, [filename])
+            shutil.copy("/var/lib/mysql/" + Get_DB() + "/" + filename,path + filename)
+            file = '/var/lib/mysql/study_db/' + filename
+            if os.path.isfile(file):
+                os.remove(file)
+        else:
+            filename = "kosdaq_invest.csv"
+            table = "Kosdaq_invest_info"
+            cursor.execute("""
+               SELECT * INTO OUTFILE %s FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\\\' LINES TERMINATED BY '\\n' FROM """ + table + """;
+              """, [filename])
+            shutil.copy("/var/lib/mysql/" + Get_DB() + "/" + filename, path + filename)
+            file = '/var/lib/mysql/study_db/' + filename
+            if os.path.isfile(file):
+                os.remove(file)
     sector /= 2
     sector = int(sector)
-    if (sector % 2 is 1):
-        filename = "konex_invest.csv"
-        table = "Konex_invest_info"
-        cursor.execute("""
-           SELECT * INTO OUTFILE %s FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\\\' LINES TERMINATED BY '\\n' FROM """ + table + """;
-          """, [filename])
-        shutil.copy("/var/lib/mysql/" + Get_DB() + "/" + filename,                                         "/workspace/stock_project3/execl/" + str(date) + '_' + filename)
-        file = '/var/lib/mysql/study_db/' + filename
-        if os.path.isfile(file):
-            os.remove(file)
+    if (sector % 2 == 1):
+        if(expect == "E"):
+            filename = "konex_invest.csv"
+            table = "Konex_exp_invest_info"
+            cursor.execute("""
+               SELECT * INTO OUTFILE %s FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\\\' LINES TERMINATED BY '\\n' FROM """ + table + """;
+              """, [filename])
+            shutil.copy("/var/lib/mysql/" + Get_DB() + "/" + filename, path + filename)
+            file = '/var/lib/mysql/study_db/' + filename
+            if os.path.isfile(file):
+                os.remove(file)
+        else:
+            filename = "konex_invest.csv"
+            table = "Konex_invest_info"
+            cursor.execute("""
+               SELECT * INTO OUTFILE %s FIELDS TERMINATED BY ',' ENCLOSED BY '"' ESCAPED BY '\\\\' LINES TERMINATED BY '\\n' FROM """ + table + """;
+              """, [filename])
+            shutil.copy("/var/lib/mysql/" + Get_DB() + "/" + filename,path + filename)
+            file = '/var/lib/mysql/study_db/' + filename
+            if os.path.isfile(file):
+                os.remove(file)
     con.close()
 
 
 if __name__ == "__main__":
     if(len(sys.argv) == 0):
-        print("plese set two argument :: kospi -> 1 + kosdaq -> 2 + konex -> 4 / data ex)20200831")
-    elif(len(sys.argv) == 3):
-        Get_invest_csv(int(sys.argv[1]),sys.argv[2])
+        print("plese set three argument :: kospi -> 1 + kosdaq -> 2 + konex -> 4 / P, E/ data_name ex)20200831")
+    elif(len(sys.argv) == 4):
+        Get_invest_csv(int(sys.argv[1]),sys.argv[2],sys.argv[3])
     else:
-        print("plese set two argument :: kospi -> 1 + kosdaq -> 2 + konex -> 4 / data ex)20200831")
+        print("plese set three argument :: kospi -> 1 + kosdaq -> 2 + konex -> 4 / P, E / data_name ex)20200831")
         
 
 
